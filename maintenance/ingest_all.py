2ec1cfe4913dcc1b7b74828a14338fadba450301
JohannesSchnell <j.schnell@52north.org>
2025-09-02 14:22:11 +0200

    changed max workers = 10; added maintenance script for data ingestion

---
+++ maintenance/ingest_all.py
@@ -0,0 +1,79 @@
+import os
+import requests
+import time
+
+api_url_base = "http://api.atrai.bike"
+token = "token"
+
+ingestion_dict = {
+    "road_network": {
+        "arnsberg":
+            [{"city": "Arnsberg", "county": "Hochsauerlandkreis", "country": "Germany"}],
+        "greifswald":
+            [{"city": "Greifswald", "country": "Germany"}],
+        "saopaulo":
+            [{"city": "Sao Paulo", "country": "Brazil"}],
+        "muenster":
+            [{"city": "Münster", "country": "Germany"}],
+        "wiesbaden":
+            [{"city": "Wiesbaden", "country": "Germany"},
+             {"city": "Mainz", "country": "Germany"}],
+    }
+}
+# 'osem_data_ingestion' needs to run anyways always
+processes = [
+    'road_network',
+    'distances',
+    'statistics',
+    'bumpy-roads',
+    'dangerous-places',
+    'speed-traffic-flow'
+]
+campaigns = [
+    'arnsberg',
+    'greifswald',
+    'saopaulo',
+    'muenster',
+    'wiesbaden'
+]
+
+print(f"starting with osem_data_ingestion")
+endpoint = os.path.join(api_url_base, f"processes/osem_data_ingestion/execution?f=json")
+payload = {
+    "inputs": {
+        "token": token
+    }
+}
+try:
+    requests.post(endpoint, json=payload).raise_for_status()
+    print("osem_data_ingestion successful")
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+
+for campaign in campaigns:
+    for process in processes:
+        endpoint = os.path.join(api_url_base, f"processes/{process}/execution?f=json")
+        if process == "road_network":
+            payload = {
+                "inputs": {
+                    "campaign": campaign,
+                    "token": token,
+                    "location": ingestion_dict["road_network"][campaign]
+                }
+            }
+
+        else:
+            payload = {
+                "inputs": {
+                    "campaign": campaign,
+                    "token": token,
+                    "col_create": True
+                }
+            }
+        print(f"campaign: '{campaign}', process: '{process}'")
+        try:
+            requests.post(endpoint, json=payload).raise_for_status()
+            time.sleep(2)
+            print(f"ingestions on '{endpoint}' for '{campaign}' successful")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
